2c28e2c21fc3b46af26727ad0659a17beff59d67
Ubuntu <ubuntu@ip-172-31-24-229.us-east-2.compute.internal>
2021-02-25 16:21:18 +0000

    新規投稿機能を追加

--- db/migrate/20210225140837_create_tasks.rb
+++ db/migrate/20210225140837_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.string :contentrails 
+      t.string :content
       t.timestamps
     end
   end
