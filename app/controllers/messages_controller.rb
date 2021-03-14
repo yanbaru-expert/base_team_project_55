4d096d4edfe9781ba07329f7d8141aeb2e8cbccf
YOU <youmaya310@gmail.com>
2021-03-14 10:15:49 +0900

    new,create実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,16 @@
 class MessagesController < ApplicationController
-  
+  def new
+    @post = Post.new
+  end
+
+  def Create
+    post = Post.create!(post_params)
+    redirect_to post
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title. :content)
+  end
 end
